f8ab8d52172397dbf69a691a311cc602b161fd11
yangjiayi01 <106996809+yangjiayi01@users.noreply.github.com>
2026-08-07 22:46:51 +0800

    Upload src/ops/argmax/op.cpp

--- src/ops/argmax/op.cpp
+++ src/ops/argmax/op.cpp
@@ -1,7 +1,38 @@
 #include "op.hpp"
 
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/argmax_cpu.hpp"
+#include "../nvidia_common/nvidia_ops.hpp"
+
 namespace llaisys::ops {
 void argmax(tensor_t max_idx, tensor_t max_val, tensor_t vals) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(max_idx, max_val, vals);
+    CHECK_ARGUMENT(vals->ndim() == 1, "Argmax: vals must be 1-D.");
+    ASSERT(max_idx->isContiguous() && max_val->isContiguous() && vals->isContiguous(),
+           "Argmax: all tensors must be contiguous.");
+
+    if (max_val->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::argmax(max_idx->data(), max_val->data(), vals->data(),
+                           vals->dtype(), vals->numel());
+    }
+
+    llaisys::core::context().setDevice(max_val->deviceType(), max_val->deviceId());
+    switch (max_val->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::argmax(max_idx->data(), max_val->data(), vals->data(),
+                           vals->dtype(), vals->numel());
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA: {
+        auto stream = llaisys::core::context().runtime().stream();
+        nvidia::argmax(max_idx->data(), max_val->data(), vals->data(), vals->dtype(),
+                       vals->numel(), stream);
+        return;
+    }
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
